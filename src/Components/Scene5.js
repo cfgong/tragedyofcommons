369674bfb56debb35e915443c1c50691f1f7025e
AshuGuptaCollege <ashuguptacollege@gmail.com>
2019-06-07 17:48:38 -0500

    Working on the middle section

--- src/Components/Scene5.js
+++ src/Components/Scene5.js
@@ -5,11 +5,16 @@ import * as d3 from "d3-3";
 import $ from "jquery";
 import pond from "./lake1.png";
 import fish_url from "./fish3.png";
-import fisherman_url from "./fisherman1.png";
+import fisherman_url_1 from "./fisherman1.png";
+import fisherman_url_2 from "./fisherman.png";
 
 const divStyle = {
   width: '75%'
 };
+
+const linkStyle = {
+  color: 'white'
+}
 
 class Scene5 extends React.Component {
   constructor(props) {
@@ -26,16 +31,24 @@ componentDidMount() {
 }
 
 render() {
-  function drawPond() {
-    var svg = d3
-      .select("#pond")
-      .append("svg")
-      .attr({
-        width: 800,
-        height: 500,
-        border: "1px solid #ccc"
-      });
+  function initializeSVG() {
+    if (d3.select("svg")[0][0] == null) {
+      var svg = d3
+       .select("#pond")
+       .append("svg")
+       .attr({
+         width: 800,
+         height: 500,
+         border: "1px solid #ccc"
+       });
+       return svg;
+    }
+    return d3.select("svg");
+  }
 
+
+  function drawPond() {
+    var svg = initializeSVG();
     svg
       .append("svg:image")
       .attr("xlink:href", pond)
@@ -44,26 +57,80 @@ render() {
       .attr("width", 800)
       .attr("height", 600);
 
-      var x_locations = [225, 700, 425, 350, 600, 550, 705, 500, 425, 300];
-      var y_locations = [350, 320, 335, 300, 300, 375, 400, 300, 390, 375];
-      const picture_height = 50;
-      const picture_width = 50;
-      var i = 0;
-
-
-      // Start off by drawing a pond with 8 fish
-      for (i = 0; i < 10; i++) {
-        var fish_id = "fish" + i;
+      var fisherman_id = "#fisher1";
+      if (svg.select(fisherman_id)[0][0] == null) {
+        // Draw the fisherman
         svg
           .append("svg:image")
-          .attr("xlink:href", fish_url)
-          .attr("x", x_locations[i])
-          .attr("y", y_locations[i])
-          .attr("width", picture_width)
-          .attr("height", picture_height)
-          .attr("id", fish_id);
+          .attr("xlink:href", fisherman_url_1)
+          .attr("x", 200)
+          .attr("y", 300)
+          .attr("width", 100)
+          .attr("height", 300)
+          .attr("id", "fisher1");
       }
 
+      fisherman_id = "#fisher2";
+      if (svg.select(fisherman_id)[0][0] == null) {
+        // Draw the fisherman
+        svg
+          .append("svg:image")
+          .attr("xlink:href", fisherman_url_2)
+          .attr("x", 600)
+          .attr("y", 300)
+          .attr("width", 100)
+          .attr("height", 300)
+          .attr("id", "fisher2");
+      }
+
+
+  }
+
+  function drawPlayers() {
+    var svg = initializeSVG();
+    var x_locations = [225, 700, 425, 350, 600, 550, 705, 500, 425, 300];
+    var y_locations = [350, 320, 335, 300, 300, 375, 400, 300, 390, 375];
+    const picture_height = 50;
+    const picture_width = 50;
+    var i = 0;
+
+
+    // Start off by drawing a pond with 8 fish
+    for (i = 0; i < 10; i++) {
+      var fish_id = "fish" + i;
+      svg
+        .append("svg:image")
+        .attr("xlink:href", fish_url)
+        .attr("x", x_locations[i])
+        .attr("y", y_locations[i])
+        .attr("width", picture_width)
+        .attr("height", picture_height)
+        .attr("id", fish_id);
+    }
+
+  }
+
+  function goFish() {
+    var svg = initializeSVG();
+    var x_locations = [225, 700, 425, 350, 600, 550, 705, 500, 425, 300];
+    var y_locations = [350, 320, 335, 300, 300, 375, 400, 300, 390, 375];
+    var fisherman_id = "#fisher1";
+    for (var i = 0; i < 10; i++) {
+      var fish_id = "#fish" + i;
+      var total_delay = 1500 * (i + 1) + 1500;
+      if (svg.select(fish_id)[0][0] != null) {
+        $(fish_id)
+          .delay(1500 * i)
+          .fadeOut(1500, function() {
+            $(this).remove();
+          });
+        var xpos = "" + x_locations[i];
+        var ypos = y_locations[i] - 100;
+        var ypos_string = "" + ypos;
+        $(fisherman_id).animate({ x: xpos, y: ypos_string }, 1500);
+      }
+    }
+    $(fisherman_id).animate({ x: 200, y: 250 }, 1500);
   }
 
   return (
@@ -72,23 +139,20 @@ render() {
         <h1>Explanation of Tragedy of the Commons</h1>
         <div style={divStyle}>
         <p>
-
-        Our simple fishing game was an example of the tragedy of commons. Because
-         parties were repeatedly being selfish, overtime the lake became depleted.
-         The fishing supply diminished and disappeared altogether. <br/>
-        In the tragedy of commons, there is <a href="#" onClick={drawPond}>one shared resource</a>
-         among players.
-        Players, as rational beings, each seek to maximize his personal gain.
-        In our Common Lake example, each fisherman will consider the incurred
-        utility of harvesting one additional fish. There is a both a positive
-        and negative component. The positive utilty is the utility gained from
-        acquiring the extra fish (nearly +1), while the negative component is
-        the additional overfishing of one more animal. The effects of overfishing
-        will be shared by all the fishermen, since Common Lake is a shared resource,
-        therefore the negative effect will only be a fractional part of 1.
-        Looking at the short run, and summing together these partial utilities,
-        the fisherman can conclude that the best course of action is continually
-        fish more animals. However, this is is also the action by the other rational fishermen. <br/>
+        Our simple fishing game served to illustrate Tragedy of the Commons.
+        The increased fishermen in the town results in increased fishing.
+        The lake loses the ability to replenish itself, and the fish supply disappears,
+         and families lose their source of livelihood. This tragedy arises from the
+          conflict among <a href="#" onClick={drawPond} style={linkStyle}><b>individuals</b></a> with
+          selfish interests sharing a <a href="#" onClick={drawPlayers} style={linkStyle}><b>scarce resource.</b></a> Here,
+           multiple parties are sharing a common good, so their own decisions
+           affect the group as a whole.
+           <br/>
+           In our fishing game example, each fisherman is a rational player so wants to maximize his own utility,
+           so will fish more whenever possible. (Even if you weren’t completely selfish in your fishing decisions,
+              the other fishermen were). However, each additional fish fished degrades the quality of Common Lake,
+               and the reduces the number of fish available in the next fishing season. In a selfish situation,
+               all fishermen would seek to <a href="#" onClick={goFish} style={linkStyle}><b>maximize his own harvest of fish.</b></a>
            </p>
            </div>
            <div id="pond"> </div>
--- src/Components/Scene6.js
+++ src/Components/Scene6.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
+import formula1 from "./formula1.png";
+import formula2 from "./formula2.png";
+import formula3 from "./formula3.png";
 
 const divStyle = {
   width: '75%'
 };
+
+const imgStyle = {
+  height: '50px',
+  display: 'block',
+  margin: '0 auto'
+}
 
 function Scene6() {
   return (
@@ -13,19 +22,22 @@ function Scene6() {
         <h1>Explanation of Tragedy of the Commons</h1>
         <div style={divStyle}>
         <p>
-        In the Common Lake fishing example, the increased fishermen in the town
-        results in increased fishing. The lake loses the ability to replenish
-        itself, and the fish supply disappears, and families lose their source
-         of livelihood. Here, the social and private incentives differ.
-         To avoid the tragic destruction of the fish ecosystem, all the fishermen
-         must cooperate and reduce their collect fishing. However,
-         a single fishermen has limited incentive to reduce their own fishing,
-          because each fishermen’s harvest is only part of the problem.
-          Each fishermen utiltizes the common resource, but by overutilizing such a resource,
-          he diminishes the utility that the other fishermen can get. <br/>
-          The town could have implemented the problem in various ways.
-          This means regulating the number of fish, limit the number of fishing
-           boat permits, divide up the fishing water, etc.
+        Let us formalize this model now to demonstrate the positive and negative effects of
+         increasing an extra fish. Let n be the number of fisherman, each able to choose the
+          amount of fish they can fish. Let K be the total amount of fish available.
+           Each player i = 1, …, n chooses the amount of fish to to fish. Let k<sub>i</sub> denote
+           the amount fished by the i-th player. Then the amount of fish left in the pool,
+            after all the fishermen have fished is: <br/>
+            <img src={formula1} style={imgStyle}></img>
+            For simplicity, let the utility functions be logarithms. Player i incurs a
+            benefit equal to ln(k<sub>i</sub>) for fishing k<sub>i</sub> fish. Each player also enjoys an added
+            benefit, from the remaining fish, sustaining the ecosystem. This gives us an
+            additional benefit of: <br/>
+            <img src={formula2} style={imgStyle}></img>
+          Then, the value for each player for player i from fishing k<sub>i</sub> fish contingent on
+          the actions of the other players is given by the utility function:
+          <img src={formula3} style={imgStyle}></img>
+          where k<sub>-i</sub> are the actions of all the other players.
            </p>
            </div>
 
